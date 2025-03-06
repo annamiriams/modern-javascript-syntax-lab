fee952ac6f7a5d5e77b3a6a2b34f16438ceff049
Anna Steckel <annaextrasteckel@gmail.com>
2025-03-05 20:35:28 -0800

    exercise 9 complete

--- app.js
+++ app.js
@@ -138,3 +138,19 @@ function simpleSentence (noun = 'cat', adjective = 'orange') {
 simpleSentence();
 simpleSentence('dog', 'brown');
 
+// ! Exercise 9:
+// a. Convert the following `if...else` statement into a ternary:
+//
+//    if (pizza === 'tasty') {
+//      console.log('yum');
+//    } else {
+//      console.log('yuck');
+//    }
+//
+// Starting code (don't modify this):
+
+const pizza = 'tasty';
+
+// Your code here:
+
+pizza === 'tasty' ? console.log('yum') : console.log('yuck');
